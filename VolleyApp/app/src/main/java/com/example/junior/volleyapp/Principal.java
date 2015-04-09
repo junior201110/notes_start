79cd38d1b4323e0b0650873d1615706ca49c608f
junior201110 <junior201110@gmail.com>
2015-04-09 18:00:12 -0300

    Final dia 9

--- VolleyApp/app/src/main/java/com/example/junior/volleyapp/Principal.java
+++ VolleyApp/app/src/main/java/com/example/junior/volleyapp/Principal.java
@@ -33,8 +33,8 @@ public class Principal extends ActionBarActivity {
 
     private EditText edtNl, edtSenha;
     private ProgressBar progressBar;
-    //private  String url = "http://webserverandroid.esy.es/inicio/post/";
-    private  String url = "http://192.168.43.213/nef/noivosemfesta/index.php/inicio/post/";
+    private  String url = "http://webserverandroid.esy.es/inicio/post/";
+    //private  String url = "http://192.168.43.213/nef/noivosemfesta/index.php/inicio/post/";
     private RequestQueue rq ;
     private Map<String, String> params;
     private Gson json;
