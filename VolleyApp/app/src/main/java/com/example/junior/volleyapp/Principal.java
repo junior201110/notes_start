bef61ab1bceefb28afa3e7cf9ba8b6536aba9d57
junior201110 <junior201110@gmail.com>
2015-04-24 13:48:44 -0300

    Commit do dia 24 - IFET-RP

--- VolleyApp/app/src/main/java/com/example/junior/volleyapp/Principal.java
+++ VolleyApp/app/src/main/java/com/example/junior/volleyapp/Principal.java
@@ -1,17 +1,41 @@
 package com.example.junior.volleyapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Principal extends Activity {
+
+    ImageButton imageButton;
+    TextView    txtLogin;
+    TextView    pswSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super .onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        txtLogin = (TextView) findViewById(R.id.txtLogin);
+        pswSenha = (TextView) findViewById(R.id.txtSenha);
+
+        imageButton =(ImageButton) findViewById(R.id.sendUser);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(Principal.this);
+                alert.setTitle("ALERT");
+                alert.setIcon(R.drawable.ic_account_circle_white_48dp);
+                alert.setMessage("CLICADO ->"+ txtLogin.getText().toString()+" / "+pswSenha.getText().toString());
+                alert.show();
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
