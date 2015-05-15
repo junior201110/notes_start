1f9f1f8482862cf3d0b67688dd9ccfcf0731f53b
junior201110 <junior201110@gmail.com>
2015-05-15 17:52:57 -0300

    dia 15 - LOJA

--- VolleyApp/app/src/main/java/com/example/junior/volleyapp/NovoPedido.java
+++ VolleyApp/app/src/main/java/com/example/junior/volleyapp/NovoPedido.java
@@ -1,115 +1,24 @@
 package com.example.junior.volleyapp;
 
-import android.content.Context;
-import android.util.Log;
+import android.app.Activity;
+import android.os.Bundle;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
-import com.example.junior.volleyapp.conexao.CustomJsonArraytRequest;
+import com.example.junior.volleyapp.model.NovoPedidoModel;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 
-import java.util.HashMap;
-import java.util.Map;
+public class NovoPedido extends Activity {
 
-/**
- * Created by junior on 09/05/15.
- */
-public class NovoPedido {
-    private String url = "";
-    private Map<String,String> params;
-    private String desc;
-    private String produto;
-    private String nnotas;
-    private String data;
-    private String iduser;
-    private RequestQueue rq;
+    NovoPedidoModel  novo = new NovoPedidoModel();
 
-    public NovoPedido(){}
-    public NovoPedido(Context context,String desc, String produto, String nnotas, String data, String iduser){
-        this.setDesc(desc);
-        this.setProduto(produto);
-        this.setNnotas(nnotas);
-        this.setData(data);
-        this.setIduser(iduser);
-
-        rq = Volley.newRequestQueue(context);
-    }
-    private String insertUser(){
-
-        final String[] res = {""};
-
-        params = new HashMap<String, String>();
-        params.put("desc",getDesc());
-        params.put("produtos",getProduto());
-        params.put("nnotas",getNnotas());
-        params.put("data",getData());
-        params.put("iduser",getIduser());
-
-        CustomJsonArraytRequest jar = new CustomJsonArraytRequest(Request.Method.POST, url, params,
-                new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                try {
-                    res[0] = String.valueOf(response.get(0));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                Log.i("INSERT ERROR", volleyError.toString());
-            }
-        });
-
-        jar.setTag("novo");
-        rq.add(jar);
-
-        return res[0];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_pedido);
     }
 
-    public String getDesc() {
-        return desc;
+    private void passerData() throws Exception{
+
     }
 
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
 
-    public String getProduto() {
-        return produto;
-    }
-
-    public void setProduto(String produto) {
-        this.produto = produto;
-    }
-
-    public String getNnotas() {
-        return nnotas;
-    }
-
-    public void setNnotas(String nnotas) {
-        this.nnotas = nnotas;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String getIduser() {
-        return iduser;
-    }
-
-    public void setIduser(String iduser) {
-        this.iduser = iduser;
-    }
 }
